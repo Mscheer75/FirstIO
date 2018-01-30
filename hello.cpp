21c7d25814f5c396c0fc8ab9c8bb1bd0dfaae6bd
Matthew S Scheer <mscheer1@mail.csuchico.edu>
2018-01-30 11:19:20 -0800

    changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,17 @@
+/*matthew scheer
+1/30/2018
+csci111
+I figured this would be hello world, i geusss not
+*/
+
+
+
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hello world, my name is program-bot, What's your first name?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
